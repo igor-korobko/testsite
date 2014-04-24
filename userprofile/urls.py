a75b73f9037edbf8defefbb77348d9ab279a740d
user <kis99999@gmail.com>
2014-04-24 17:02:31 +0300

    1

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from userprofile import views
 
+
 urlpatterns = patterns('',
     url(r'^user/$', views.user_, name='user'),
     url(r'^(?P<user_name>\w+)/public/$', views.public_, name='public'),
